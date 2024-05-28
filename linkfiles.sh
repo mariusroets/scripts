73f1889c63128e8afbc11179f871a82c658b28f3
Marius Roets <roetsm@eskom.co.za>
2024-05-28 08:03:58 +0200

    Merging local changes

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,4 +1,4 @@
-rm -rf ~/bin
-ln -s `pwd`/bin ~/bin
-rm -f ~/.config/i3/config
-ln -s `pwd`/conf/i3-config ~/.config/i3/config
+rm -rf ~/.local/bin
+ln -s `pwd`/bin ~/.local/bin
+rm -f ~/.i3/config
+ln -s `pwd`/conf/i3-config ~/.i3/config
