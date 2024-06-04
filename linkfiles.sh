24bd5e9d4bf9d38a538d6b0c45e3291d3cb0d8c2
Marius Roets <roetsm@eskom.co.za>
2024-06-04 12:15:40 +0200

    Also add tmux config to linkfiles

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
 rm -rf ~/.local/bin
 ln -s `pwd`/bin ~/.local/bin
+rm -f ~/.tmux.conf
 rm -f ~/.config/tmux-powerline/config.sh
 rm -f ~/.config/tmux-powerline/themes/theme.sh
+ln -s `pwd`/conf/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/tmux-powerline/themes
 ln -s `pwd`/conf/tmux-powerline-config.sh ~/.config/tmux-powerline/config.sh
 ln -s `pwd`/conf/tmux-powerline-theme.sh ~/.config/tmux-powerline/themes/theme.sh
