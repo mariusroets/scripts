206f1d6a8fbc3fd8de65651901625fc37b8e86e8
Marius Roets <roetsm@eskom.co.za>
2024-06-04 07:25:42 +0200

    Link config files

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,4 +1,8 @@
 rm -rf ~/.local/bin
 ln -s `pwd`/bin ~/.local/bin
-rm -f ~/.i3/config
-ln -s `pwd`/conf/i3-config ~/.i3/config
+rm -f ~/.config/tmux-powerline/config.sh
+rm -f ~/.config/tmux-powerline/themes/theme.sh
+mkdir -p ~/.config/tmux-powerline/themes
+ln -s `pwd`/conf/tmux-powerline-config.sh ~/.config/tmux-powerline/config.sh
+ln -s `pwd`/conf/tmux-powerline-theme.sh ~/.config/tmux-powerline/themes/theme.sh
+
