eb858a23eedb17880d92756d2425e5ab6d264ee1
Marius Roets <roets.marius@gmail.com>
2024-06-10 09:05:34 +0200

    Link NeoVim config

--- linkfiles.sh
+++ linkfiles.sh
@@ -9,4 +9,6 @@ ln -s `pwd`/conf/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/tmux-powerline/themes
 ln -s `pwd`/conf/tmux-powerline-config.sh ~/.config/tmux-powerline/config.sh
 ln -s `pwd`/conf/tmux-powerline-theme.sh ~/.config/tmux-powerline/themes/theme.sh
+rm -f ~/.config/nvim
+ln -s `pwd`/conf/nvim ~/.config/nvim
 
