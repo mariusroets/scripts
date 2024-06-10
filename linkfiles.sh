b652d85ade283457ee63251d225a1dc77a603485
Marius Roets <roets.marius@gmail.com>
2024-06-10 10:24:09 +0200

    More flexible config linking

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+for dir in `pwd`/conf/*/; do
+    config=$(basename $dir)
+    echo "Copying config for: $config"
+    rm -rf ~/.config/$config
+    ln -s $dir ~/.config
+done
 rm -rf ~/.local/bin
 ln -s `pwd`/bin ~/.local/bin
 rm -f ~/.tmux.conf
-rm -f ~/.config/tmux-powerline/config.sh
-rm -f ~/.config/tmux-powerline/themes/theme.sh
 ln -s `pwd`/conf/.tmux.conf ~/.tmux.conf
-mkdir -p ~/.config/tmux-powerline/themes
-ln -s `pwd`/conf/tmux-powerline-config.sh ~/.config/tmux-powerline/config.sh
-ln -s `pwd`/conf/tmux-powerline-theme.sh ~/.config/tmux-powerline/themes/theme.sh
-rm -f ~/.config/nvim
-ln -s `pwd`/conf/nvim ~/.config/nvim
 
