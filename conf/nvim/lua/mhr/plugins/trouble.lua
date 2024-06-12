e75cdbda7bdca175668dd2650643539db342d199
Marius Roets <roetsm@eskom.co.za>
2024-06-12 08:57:34 +0200

    Changed mappings for newer version

--- conf/nvim/lua/mhr/plugins/trouble.lua
+++ conf/nvim/lua/mhr/plugins/trouble.lua
@@ -1,12 +1,16 @@
 return {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+    opts = {},
+    cmd = "Trouble",
     keys = {
-        { "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
-        { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-        { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-        { "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-        { "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
-        { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
+        { "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Diagnostics (Trouble)" },
+        { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Buffer Diagnostics (Trouble)" },
+        { "<leader>xw", "<cmd>Trouble workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
+        { "<leader>xd", "<cmd>Trouble document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
+        { "<leader>xl", "<cmd>Trouble lsp toggle<CR>", desc = "LSP definitions /references under cursor" },
+        { "<leader>xQ", "<cmd>Trouble qflist toggle<CR>", desc = "Open trouble quickfix list" },
+        { "<leader>xL", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
+        { "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
     },
 }
