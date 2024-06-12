438173d1bf39769086b558afed5df763b2f5370e
Marius Roets <roetsm@eskom.co.za>
2024-06-12 08:57:19 +0200

    sqls plugin for working with SQL

--- conf/nvim/lua/mhr/plugins/init.lua
+++ conf/nvim/lua/mhr/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-lua/plenary.nvim", -- lua functions that many plugins use
     "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+    "nanotee/sqls.nvim", -- Wrapper for sql language server
 }
 --TODO: Add the following:
 -- autopairs
