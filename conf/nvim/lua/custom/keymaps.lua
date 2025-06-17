3618c0ddb83429d778c13e1eb26c4788bc409738
Marius Roets <roets.marius@gmail.com>
2025-06-17 18:16:42 +0200

    fix: Shortcuts for splitting, chatting and sessions

--- conf/nvim/lua/custom/keymaps.lua
+++ conf/nvim/lua/custom/keymaps.lua
@@ -4,8 +4,12 @@ vim.keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
 vim.keymap.set('n', '<leader>|', '<C-w>v', { desc = 'Split window vertically' }) -- split window vertically
 vim.keymap.set('n', '<leader>-', '<C-w>s', { desc = 'Split window horizontally' }) -- split window horizontally
 
+vim.keymap.set("n", "<leader>ss", "<cmd>SessionSave<CR>", { desc = "Manually Save Session" })
+vim.keymap.set("n", "<leader>sl", "<cmd>SessionSearch<CR>", { desc = "Open session loader" })
+
 vim.keymap.set('n', '<f3>', '<cmd>AerialToggle<cr>', { desc = 'Open Symbol Navigator' })
-vim.keymap.set('n', '<f4>', '<cmd>ToggleViewLayout<cr>', { desc = 'Open in split window mode' })
+vim.keymap.set('n', '<f4>', '<C-w>v', { desc = 'Open in split window mode' })
+vim.keymap.set('n', '<f5>', '<cmd>CodeCompanionChat<cr>', { desc = 'Open the Code Companion chat window' })
 vim.keymap.set('n', '<f7>', '@q', { desc = 'Run macro in the q register' })
 vim.keymap.set('n', '<f12>', '<cmd>cd ~/.config/nvim<cr><cmd>e init.lua<cr>', { desc = 'Edit NVim config' })
 vim.keymap.set('n', '<tab>', '<cmd>bnext<cr>', { desc = 'Go to the next buffer' })
