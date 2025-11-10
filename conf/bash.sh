46a3673a9caaa241ee0cb1a3a36db6cc0b9e2dfa
Marius Roets <roets.marius@gmail.com>
2025-11-10 07:11:35 +0200

    fix: Secrets

--- conf/bash.sh
+++ conf/bash.sh
@@ -28,5 +28,6 @@ source ~/scripts/conf/alias
 export POWI_ENV_PATH=/data/code/environments
 export PATH=$PATH:$POWI_ENV_PATH/bin
 export EDITOR=nvim
+. ~/.secrets
 eval "$(zoxide init bash)"
 eval "$(fzf --bash)"
