b2fe2301159efe8b2e06d39aef89c4b096299cce
Marius Roets <roets.marius@gmail.com>
2026-01-08 06:27:51 +0200

    fix: Fix for git pages default settings

--- conf/zsh.sh
+++ conf/zsh.sh
@@ -3,6 +3,8 @@ source ~/scripts/conf/alias
 export POWI_ENV_PATH=/data/code/environments
 export PATH=$PATH:$POWI_ENV_PATH/bin
 export EDITOR=nvim
+# Make 'less' exit when output is less than one screen, also handling colour codes correctly
+export LESS="-FRX"
 . ~/.secrets
 eval "$(zoxide init zsh)"
 eval "$(fzf --zsh)"
