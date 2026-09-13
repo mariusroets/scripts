2c96fbc45de3b358b22670fa5ea68f075c2aa115
Marius Roets <roets.marius@gmail.com>
2026-09-13 10:00:55 +0200

    fix: Add usql function

--- conf/bash.sh
+++ conf/bash.sh
@@ -5,3 +5,19 @@ export POWI_ENV_PATH=/data/code/platform/environments
 export PATH=$PATH:$POWI_ENV_PATH/bin
 export EDITOR=nvim
 . ~/.secrets
+
+# Custom usql wrapper function
+u() {
+    local target="$1"
+
+    case "$target" in
+        *prod*|*PROD*)
+            # RED prompt for production targets
+            usql -v PROMPT1='%[%033[1;31m%][PROD] %n@%m %#%[%033[0m%] ' "$@"
+            ;;
+        *)
+            # GREEN prompt for non-production targets
+            usql -v PROMPT1='%[%033[1;32m%][DEV] %n@%m %#%[%033[0m%] ' "$@"
+            ;;
+    esac
+}
