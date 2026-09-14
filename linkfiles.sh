fb0e05d40bca9ac3790bb14d2f48d25c5d0125a7
Marius Roets <roets.marius@gmail.com>
2026-09-14 11:36:30 +0200

    fix: New improved and safe linkfile

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
-for dir in `pwd`/conf/*/; do
-    config=$(basename $dir)
-    echo "Copying config for: $config"
-    rm -rf ~/.config/$config
-    ln -s $dir ~/.config
+CONF_DIR=~/.config
+
+confirm_action() {
+    read -p "$1 (y/N): " response
+    [[ "$response" =~ ^[Yy]$ ]]
+}
+copy_dir() {
+    if [[ -d $CONF_DIR/$1 ]]; then
+        timestamp=$(date +%Y%m%d%H%M)
+        mv $CONF_DIR/$1 $CONF_DIR/$1.$timestamp
+    fi
+    ln -s $1 $CONF_DIR
+}
+
+if confirm_action "Install NeoVim config?"; then
+    copy_dir nvim
+else
+    echo "Skipped..."
+fi
+if confirm_action "Install Lazygit config?"; then
+    copy_dir lazygit
+else
+    echo "Skipped..."
+fi
+if confirm_action "Install tmux config?"; then
+    copy_dir tmux
+else
+    echo "Skipped..."
+fi
+for f in bin/*; do
+
+    if confirm_action "Install $f"; then
+        ln -s bin/$f ~/.local/bin/$f
+    fi
 done
-rm -rf ~/.local/bin
-ln -s `pwd`/bin ~/.local/bin
-rm -f ~/.tmux.conf
-ln -s `pwd`/conf/.tmux.conf ~/.tmux.conf
+
 
