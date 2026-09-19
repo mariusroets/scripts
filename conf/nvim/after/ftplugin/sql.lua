dce6329740251a1882ac75339dcf6b6f069d23d8
Marius Roets <roets.marius@gmail.com>
2026-09-19 07:25:25 +0200

    fix: Change sql execution to S-F9

--- conf/nvim/after/ftplugin/sql.lua
+++ conf/nvim/after/ftplugin/sql.lua
@@ -24,6 +24,6 @@ vim.api.nvim_create_user_command("RunSql",
     {})
 
 vim.bo.commentstring = "-- %s"
-vim.keymap.set('n', '<F9>', '<cmd>w<cr><cmd>RunSql<cr>', { buffer = true })
-vim.keymap.set('i', '<F9>', '<esc><cmd>w<cr><cmd>RunSql<cr>', { buffer = true })
+vim.keymap.set('n', '<F21>', '<cmd>w<cr><cmd>RunSql<cr>', { buffer = true })
+vim.keymap.set('i', '<F21>', '<esc><cmd>w<cr><cmd>RunSql<cr>', { buffer = true })
 vim.keymap.set('n', '<F10>', '<cmd>set ft=plsql<cr>', { buffer = true })
